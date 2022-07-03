4fe1dea8821f60fcb2b09709442e68139c85c8be
Mathias Lovera <mathiasalejandroloverasilva@gmail.com>
2022-07-03 13:27:01 -0400

    colocando rutas a partir de un path especifico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Route, RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+//modules
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+//components
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-    ],
-  },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
-  { path: '**', component: NopagefoundComponent },
+  //path '/dashboard' PagesRoutingModule
+  //path '/auth' AuthRoutingModule
+  //path '/' PagesRoutingModule
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '**', component: NopagefoundComponent }
 ];
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), PagesRoutingModule,AuthRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
